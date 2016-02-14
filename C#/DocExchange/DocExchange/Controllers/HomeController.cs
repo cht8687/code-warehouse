5e43ee13e710f26761667bb4ee5a3df291e1f4a0
Haotian.Chang <cht8687@gmail.com>
2016-02-15 01:10:19 +1100

    Knockout study

---
+++ C#/DocExchange/DocExchange/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DocExchange.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Step1()
+        {
+            ViewBag.Message = "STEP 1";
+
+            return View();
+        }
+
+        public ActionResult Basic()
+        {
+            return View();
+        }
+
+        public ActionResult Advanced()
+        {
+            var person = new DocExchange.Models.Person
+            {
+                FirstName = "Robert",
+                LastName = "Chang"
+            };
+            return View(person);
+        }
+    }
+}
---
+++ C#/DocExchange/DocExchange/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DocExchange.MvcApplication" Language="C#" %>

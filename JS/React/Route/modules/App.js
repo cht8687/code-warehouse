d9b0d03cc74c43cb7f1f759a06049938553ee116
Haotian.Chang <cht8687@gmail.com>
2016-03-07 21:03:36 +1100

    NavLink component

--- JS/React/Route/modules/App.js
+++ JS/React/Route/modules/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router'
+import NavLink from './NavLink'
 
 export default React.createClass({
   render() {
@@ -7,8 +8,8 @@ export default React.createClass({
       <div>
         <h1>React Router Tutorial</h1>
         <ul role="nav">
-          <li><Link to="/about" activeClassName="active">About</Link></li>
-          <li><Link to="/repos" activeClassName="active">Repos</Link></li>
+          <li><NavLink to="/about" activeClassName="active">About</NavLink></li>
+          <li><NavLink to="/repos" activeClassName="active">Repos</NavLink></li>
         </ul>
         {this.props.children}
       </div>

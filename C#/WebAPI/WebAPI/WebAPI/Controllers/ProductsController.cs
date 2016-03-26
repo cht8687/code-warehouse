9618e7c55a37afcf4d688dda31cd6568d8600388
Haotian.Chang <cht8687@gmail.com>
2016-03-26 16:46:15 +1100

    controller

---
+++ C#/WebAPI/WebAPI/WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,42 @@
+﻿using APM.WebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        // GET: api/Products
+        public IEnumerable<Product> Get()
+        {
+            var productRepository = new ProductRepository();
+            return productRepository.Retrieve();
+        }
+
+        // GET: api/Products/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Products
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Products/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Products/5
+        public void Delete(int id)
+        {
+        }
+    }
+}

64c9a77a2232fd949f52a94cd27a7d7b3ffde6e5
Haotian.Chang <cht8687@gmail.com>
2016-03-27 22:00:37 +1100

    add CORS support

--- C#/WebAPI/WebAPI/WebAPI/App_Start/WebApiConfig.cs
+++ C#/WebAPI/WebAPI/WebAPI/App_Start/WebApiConfig.cs
@@ -19,7 +19,7 @@ namespace WebAPI
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+            config.EnableCors();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
--- C#/WebAPI/WebAPI/WebAPI/Controllers/ProductsController.cs
+++ C#/WebAPI/WebAPI/WebAPI/Controllers/ProductsController.cs
@@ -5,10 +5,12 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using System.Web.Http.Cors;
 using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
+    [EnableCorsAttribute("http://localhost:49612", "*", "*")]
     public class ProductsController : ApiController
     {
         // GET: api/Products

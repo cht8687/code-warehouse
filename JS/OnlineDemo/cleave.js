f43f0ab88d819976e730ba7937766ba69372c077
Haotian Chang <cht8687@gmail.com>
2016-08-28 14:45:42 +1000

    backup JSBIN demo

---
+++ JS/OnlineDemo/cleave.js
@@ -0,0 +1,18 @@
+### issue28
+
+[issue28](https://github.com/nosir/cleave.js/issues/28)
+
+http://output.jsbin.com/pasulefepu/
+
+### issue84
+
+[issue84](https://github.com/nosir/cleave.js/issues/84)
+
+http://jsbin.com/wuxogiduwe/
+
+### issue39
+
+[issue39](https://github.com/nosir/cleave.js/issues/39)
+
+http://jsbin.com/cibekidemo/1/
+

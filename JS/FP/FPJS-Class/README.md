21b94d239b79321d5d9077c4812a992a2a39e6fe
cht8687 <cht8687@gmail.com>
2016-10-07 20:15:03 +1100

    FP

---
+++ JS/FP/FPJS-Class/README.md
@@ -0,0 +1,17 @@
+FP-JS Class
+==================
+
+Functional Programming learned through JS
+
+Presentation here: http://goo.gl/zovcux
+
+**Installation**:
+`npm install`
+
+**Example of running tests**:
+Tests are all located in their cooresponding folder so you must cd in to run.
+
+```
+cd exercises/curry
+mocha .
+```
---
+++ JS/FP/FPJS-Class/part1_exercises/exercises/curry/curry_exercises.js
@@ -0,0 +1,60 @@
+require('../../support');
+var _ = require('ramda');
+
+
+// Exercise 1
+//==============
+// Refactor to remove all arguments by partially applying the function
+
+var words = _.split(' ');
+
+// Exercise 1a
+//==============
+// Use map to make a new words fn that works on an array of strings.
+
+var sentences = undefined;
+
+// Exercise 2
+//==============
+// Refactor to remove all arguments by partially applying the functions
+
+var filterQs = function(xs) {
+  return _.filter(function(x){ return _.match(/q/i, x);  }, xs);
+};
+
+
+// Exercise 3
+//==============
+// Use the helper function _keepHighest to refactor max to not reference any arguments
+
+// LEAVE BE:
+var _keepHighest = function(x,y){ return x >= y ? x : y; };
+
+// REFACTOR THIS ONE:
+var max = function(xs) {
+  return _.reduce(function(acc, x){
+    return _keepHighest(acc, x);
+  }, 0, xs);
+};
+
+  
+// Bonus 1:
+// ============
+// wrap array's built in slice to be functional and curried like fn's in ramda
+// //[1,2,3].slice(0, 2)
+var slice = undefined;
+
+
+// Bonus 2:
+// ============
+// use slice to define a function "take" that takes n elements. Make it curried
+var take = undefined;
+
+
+module.exports = { words: words,
+                   sentences: sentences,
+                   filterQs: filterQs,
+                   max: max,
+                   slice: slice,
+                   take: take
+                 };

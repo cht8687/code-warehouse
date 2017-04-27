eb07b26ce12a796a0dbfbe9ee6d07522c5ab4b59
cht8687 <cht8687@gmail.com>
2017-04-27 17:15:34 +1000

    test

--- JS/Angular/client/app/main/main.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('apiApp')
-  .config(function($stateProvider) {
-    $stateProvider.state('main', {
-      url: '/',
-      template: '<main></main>'
-    });
-  });
--- JS/Angular/client/components/navbar/navbar.directive.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-angular.module('apiApp')
-  .directive('navbar', () => ({
-    templateUrl: 'components/navbar/navbar.html',
-    restrict: 'E',
-    controller: 'NavbarController',
-    controllerAs: 'nav'
-  }));
